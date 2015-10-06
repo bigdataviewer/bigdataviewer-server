6cceb43217bc8cd1f2eb15fdcaf7a9831a1f313c
HongKee Moon <hkmoon@me.com>
2015-10-06 13:55:52 +0200

    Remove the all the credential information
    All the important information are required to have in the initial run with /manager context enabled.
    SSL certificate and jetty user property files are located under current_working_folder/etc.
    Whenever users want to update the critical information, they can modify them directly.

--- src/main/java/bdv/server/BigDataServer.java
+++ src/main/java/bdv/server/BigDataServer.java
@@ -13,20 +13,61 @@ import org.eclipse.jetty.server.handler.*;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.util.security.Constraint;
+import org.eclipse.jetty.util.security.Password;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import sun.security.x509.AlgorithmId;
+import sun.security.x509.CertificateAlgorithmId;
+import sun.security.x509.CertificateIssuerName;
+import sun.security.x509.CertificateSerialNumber;
+import sun.security.x509.CertificateSubjectName;
+import sun.security.x509.CertificateValidity;
+import sun.security.x509.CertificateVersion;
+import sun.security.x509.CertificateX509Key;
+import sun.security.x509.X500Name;
+import sun.security.x509.X509CertImpl;
+import sun.security.x509.X509CertInfo;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
+import java.security.SignatureException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.util.Date;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Serve XML/HDF5 datasets over HTTP.
@@ -113,15 +154,22 @@ public class BigDataServer
 		Handler handler = handlers;
 		if ( params.enableManagerContext() )
 		{
+			if ( !checkKeystore() )
+				throw new IllegalArgumentException( "Keystore file does not exist." );
+
+			if ( !checkRealmProperty() )
+				throw new IllegalArgumentException( "Login property file does not exist." );
+
 			final HttpConfiguration https = new HttpConfiguration();
 			https.addCustomizer( new SecureRequestCustomizer() );
 
-			// Please, change localhost according to your site name
-			// keytool -genkey -alias localhost -keyalg RSA -keystore keystore.jks -keysize 2048
 			final SslContextFactory sslContextFactory = new SslContextFactory();
-			sslContextFactory.setKeyStorePath( Resource.newClassPathResource( "etc/keystore.jks" ).toString() );
-			sslContextFactory.setKeyStorePassword( "123456" );
-			sslContextFactory.setKeyManagerPassword( "123456" );
+			sslContextFactory.setKeyStorePath( "etc/keystore.jks" );
+
+			final char passwordArray[] = System.console().readPassword( "Please, enter your keystore password: " );
+			String password = new String( passwordArray );
+			sslContextFactory.setKeyStorePassword( password );
+			sslContextFactory.setKeyManagerPassword( password );
 
 			final ServerConnector sslConnector = new ServerConnector( server,
 					new SslConnectionFactory( sslContextFactory, "http/1.1" ),
@@ -148,11 +196,11 @@ public class BigDataServer
 			constraint.setDataConstraint( Constraint.DC_CONFIDENTIAL );
 
 			final ConstraintMapping cm = new ConstraintMapping();
-			cm.setPathSpec( "/ " + Constants.MANAGER_CONTEXT_NAME + "/*" );
+			cm.setPathSpec( "/" + Constants.MANAGER_CONTEXT_NAME + "/*" );
 			cm.setConstraint( constraint );
 
 			// Please change the password in realm.properties
-			final HashLoginService loginService = new HashLoginService( "BigDataServerRealm", Resource.newClassPathResource( "etc/realm.properties" ).toString() );
+			final HashLoginService loginService = new HashLoginService( "BigDataServerRealm", "etc/realm.properties" );
 			server.addBean( loginService );
 
 			final ConstraintSecurityHandler sh = new ConstraintSecurityHandler();
@@ -173,6 +221,211 @@ public class BigDataServer
 		LOG.info( "BigDataServer starting" );
 		server.start();
 		server.join();
+	}
+
+	private static boolean checkKeystore()
+	{
+		// check if "etc/keystore.jks" exists
+		if ( Files.exists( Paths.get( "etc/keystore.jks" ) ) )
+			return true;
+		else
+		{
+			try
+			{
+				// keytool -genkey -alias localhost -keyalg RSA -keystore keystore.jks -keysize 2048
+				if ( Files.notExists( Paths.get( "etc/" ) ) )
+					Files.createDirectory( Paths.get( "etc/" ) );
+
+				KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance( "RSA" );
+				keyPairGenerator.initialize( 2048 );
+				KeyPair KPair = keyPairGenerator.generateKeyPair();
+				PrivateKey privkey = KPair.getPrivate();
+
+				X509CertInfo info = new X509CertInfo();
+				Date from = new Date();
+				Date to = new Date( from.getTime() + 365 * 86400000l );
+				CertificateValidity interval = new CertificateValidity( from, to );
+				BigInteger sn = new BigInteger( 64, new SecureRandom() );
+				X500Name owner = new X500Name( "CN=Unknown, L=Unknown, ST=Unknown, O=Unknown, OU=Unknown, C=Unknown" );
+
+				info.set( X509CertInfo.VALIDITY, interval );
+				info.set( X509CertInfo.SERIAL_NUMBER, new CertificateSerialNumber( sn ) );
+				boolean justName = isJavaAtLeast( 1.8 );
+				if ( justName )
+				{
+					info.set( X509CertInfo.SUBJECT, owner );
+					info.set( X509CertInfo.ISSUER, owner );
+				}
+				else
+				{
+					info.set( X509CertInfo.SUBJECT, new CertificateSubjectName( owner ) );
+					info.set( X509CertInfo.ISSUER, new CertificateIssuerName( owner ) );
+				}
+
+				info.set( X509CertInfo.KEY, new CertificateX509Key( KPair.getPublic() ) );
+				info.set( X509CertInfo.VERSION, new CertificateVersion( CertificateVersion.V3 ) );
+				AlgorithmId algo = new AlgorithmId( AlgorithmId.sha256WithRSAEncryption_oid );
+				info.set( X509CertInfo.ALGORITHM_ID, new CertificateAlgorithmId( algo ) );
+
+				// Sign the cert to identify the algorithm that's used.
+				X509CertImpl cert = new X509CertImpl( info );
+				cert.sign( privkey, "SHA256withRSA" );
+
+				// Update the algorithm, and resign.
+				algo = ( AlgorithmId ) cert.get( X509CertImpl.SIG_ALG );
+				info.set( CertificateAlgorithmId.NAME + "." + CertificateAlgorithmId.ALGORITHM, algo );
+				cert = new X509CertImpl( info );
+				cert.sign( privkey, "SHA256withRSA" );
+
+				KeyStore keyStore = null;
+				FileOutputStream keyStoreFile = null;
+
+				// Load the default Java keystore
+				keyStore = KeyStore.getInstance( KeyStore.getDefaultType() );
+				keyStore.load( null, "changeit".toCharArray() );
+
+				final char passwordArray[] = System.console().readPassword( "Enter your new keystore password for SSL connection: " );
+
+				String password = new String( passwordArray );
+
+				// Put our information
+				keyStore.setCertificateEntry( "localhost", cert );
+				keyStore.setKeyEntry( "localhost", privkey,
+						password.toCharArray(),
+						new java.security.cert.Certificate[] { cert } );
+
+				// Generate new cert
+				keyStoreFile = new FileOutputStream( "etc/keystore.jks" );
+				keyStore.store( keyStoreFile, password.toCharArray() );
+				keyStoreFile.close();
+			}
+			catch ( FileNotFoundException e )
+			{
+				e.printStackTrace();
+				return false;
+			}
+			catch ( KeyStoreException e )
+			{
+				e.printStackTrace();
+				return false;
+			}
+			catch ( IOException e )
+			{
+				e.printStackTrace();
+				return false;
+			}
+			catch ( NoSuchAlgorithmException e )
+			{
+				e.printStackTrace();
+				return false;
+			}
+			catch ( CertificateException e )
+			{
+				e.printStackTrace();
+				return false;
+			}
+			catch ( SignatureException e )
+			{
+				e.printStackTrace();
+				return false;
+			}
+			catch ( NoSuchProviderException e )
+			{
+				e.printStackTrace();
+				return false;
+			}
+			catch ( InvalidKeyException e )
+			{
+				e.printStackTrace();
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static final Pattern JAVA_VERSION = Pattern.compile( "([0-9]*.[0-9]*)(.*)?" );
+
+	/**
+	 * Checks whether the current Java runtime has a version equal or higher then the given one. As Java version are
+	 * not double (because they can use more digits such as 1.8.0), this method extracts the two first digits and
+	 * transforms it as a double.
+	 * @param version the version
+	 * @return {@literal true} if the current Java runtime is at least the specified one,
+	 * {@literal false} if not or if the current version cannot be retrieve or is the retrieved version cannot be
+	 * parsed as a double.
+	 */
+	public static boolean isJavaAtLeast( double version )
+	{
+		String javaVersion = System.getProperty( "java.version" );
+		if ( javaVersion == null )
+		{
+			return false;
+		}
+
+		// if the retrieved version is one three digits, remove the last one.
+		Matcher matcher = JAVA_VERSION.matcher( javaVersion );
+		if ( matcher.matches() )
+		{
+			javaVersion = matcher.group( 1 );
+		}
+
+		try
+		{
+			double v = Double.parseDouble( javaVersion );
+			return v >= version;
+		}
+		catch ( NumberFormatException e )
+		{
+			return false;
+		}
+	}
+
+	private static boolean checkRealmProperty()
+	{
+		Path path = Paths.get( "etc/realm.properties" );
+		// check if "etc/realm.properties" exists
+		if ( Files.exists( path ) )
+			return true;
+		else
+		{
+			try
+			{
+				if ( Files.notExists( Paths.get( "etc/" ) ) )
+					Files.createDirectory( Paths.get( "etc/" ) );
+
+				final String userId = System.console().readLine( "Enter your ID for manager : " );
+				final char passwordArray[] = System.console().readPassword( "Enter your password for \"%s\": ", userId );
+
+				String md5 = Password.MD5.digest( new String( passwordArray ) );
+
+				BufferedWriter writer = Files.newBufferedWriter( path, StandardCharsets.UTF_8 );
+
+				writer.append( "#   http://www.eclipse.org/jetty/documentation/current/configuring-security-secure-passwords.html\n" );
+				writer.append( "#   Please, use obfuscated/MD5/crypted password only by using below instructions\n" );
+				writer.append( "#\n" );
+				writer.append( "# \t$ export JETTY_VERSION=9.0.0.RC0\n" );
+				writer.append( "# \t$ java -cp lib/jetty-util-$JETTY_VERSION.jar org.eclipse.jetty.util.security.Password username blahblah\n" );
+				writer.append( "# \tOBF:20771x1b206z\n" );
+				writer.append( "# \tMD5:639bae9ac6b3e1a84cebb7b403297b79\n" );
+				writer.append( "# \tCRYPT:me/ks90E221EY\n" );
+
+				writer.newLine();
+				writer.append( userId );
+				writer.append( ": " );
+				writer.append( md5 );
+				writer.append( ", admin" );
+				writer.newLine();
+
+				writer.flush();
+				writer.close();
+			}
+			catch ( IOException e )
+			{
+				e.printStackTrace();
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/**
